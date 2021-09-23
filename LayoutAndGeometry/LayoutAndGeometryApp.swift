61cad234cdc73f64cc212e40f11874aa2a344a3d
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-09-22 18:03:27 -0700

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Sergio Sepulveda on 2021-09-22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
